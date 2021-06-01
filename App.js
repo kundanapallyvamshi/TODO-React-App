8b5ed6d99799f26260031a0f84059fcf03b2e40f
wamsi316 <wamsi316@gmail.com>
2021-06-01 12:04:50 +0530

    on adding task keyboard dismissal

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, FlatList,TextInput,Button,Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList,TextInput,Button,Alert,TouchableWithoutFeedback,Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoitem';
 import AddTodo from './components/addTodo';
@@ -36,6 +36,9 @@ export default function App() {
 
   }
   return (
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.dismiss();
+    }}>
     <View style={styles.container}>
       <Header/>
       <View style={styles.content}>
@@ -53,7 +56,8 @@ export default function App() {
         </View>
       </View>
 
-    </View>  
+    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
